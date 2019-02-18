c879fa45ad44de8a563d62cbc840ba7a7170aa2c
EmersonYe <emersonye@gmail.com>
2019-02-17 17:51:13 -0800

    Setup script

---
+++ tutorial/setup.sh
@@ -0,0 +1,10 @@
+if ! [ -d scripts ]; then
+	echo "Scripts folder not detected. Downloading..."
+	curl -LOks https://github.com/EmersonYe/Earl-Grey/raw/master/scripts.zip
+	unzip -q scripts.zip
+	rm -f scripts.zip
+	echo "Scripts folder successfully downloaded."
+else
+	echo "Scripts folder detected. Skipping download."
+fi
+
